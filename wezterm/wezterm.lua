40ee252498d8e802dc87ce61b3f68875094d3dcc
Artem Medeu <artem.medeusheyev@gmail.com>
2022-10-05 14:57:42 +0600

    wezterm window config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,4 +6,17 @@ return {
   tab_bar_at_bottom = true,
   hide_tab_bar_if_only_one_tab = true,
   color_scheme = 'nightfox',
+  window_frame = {
+    border_left_width = '0.5cell',
+    border_right_width = '0.5cell',
+    border_bottom_height = '0.25cell',
+    border_top_height = '0.25cell',
+  },
+  -- window_decorations = 'NONE',
+  window_padding = {
+    left = 15,
+    right = 15,
+    top = 15,
+    bottom = 0,
+  }
 }
