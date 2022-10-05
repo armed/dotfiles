9bbbfdb1087febf3d6314071bd0397b00d12b463
Artem Medeu <artem.medeusheyev@gmail.com>
2022-10-05 14:48:15 +0600

    wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font 'JetBrainsMono Nerd Font Mono',
+  font_size = 16.0,
+  tab_bar_at_bottom = true,
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = 'nightfox',
+}
