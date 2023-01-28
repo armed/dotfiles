f6ab12ef4dd78a582f7ebd9dc5ad4f3871b14a6e
Artem Medeu <artem.medeusheyev@gmail.com>
2023-01-29 00:54:08 +0600

    nvim: autocommands

--- nvim/lua/config/aucommands.lua
+++ nvim/lua/config/aucommands.lua
@@ -1,17 +1,4 @@
 local user_group = vim.api.nvim_create_augroup('UserGroup', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = user_group,
-  pattern = '*',
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'help', 'man' },
-  command = 'noremap <buffer> q <cmd>quit<cr>',
-  group = user_group
-})
 
 local function set_terminal_keymaps()
   local opts = { buffer = 0 }
@@ -28,3 +15,62 @@ vim.api.nvim_create_autocmd('TermOpen', {
   callback = set_terminal_keymaps
 })
 
+-- This file is automatically loaded by plugins.init
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd(
+  { "FocusGained", "TermClose", "TermLeave" },
+  { command = "checktime" }
+)
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "qf",
+    "help",
+    "man",
+    "notify",
+    "lspinfo",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "PlenaryTestPopup",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
