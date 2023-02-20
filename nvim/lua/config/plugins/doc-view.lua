87270aeaf4dab57e2d1b1830be655ab9e272ccf5
Artem Medeu <artem.medeusheyev@gmail.com>
2023-02-20 11:56:11 +0600

    nvim: pin docs-view

--- nvim/lua/config/plugins/doc-view.lua
+++ nvim/lua/config/plugins/doc-view.lua
@@ -1,9 +1,10 @@
 return {
   "amrbashir/nvim-docs-view",
   cmd = "DocsViewUpdate",
+  commit = "601d7f6a2e399226a669fd2a73f7da77726cb32f",
   opts = {
     position = "right",
     width = 80,
     update_mode = "manual"
-  }
+  },
 }
