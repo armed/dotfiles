ae4676e79469faa88b3ef52aa4438ec1476359c0
Artem Medeu <artem.medeusheyev@gmail.com>
2023-03-07 22:32:15 +0600

    nvim: fzf->telescope

--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -4,6 +4,7 @@ local M = {
   branch = "0.1.x",
   dependencies = {
     "tsakirist/telescope-lazy.nvim",
+    "aaronhallaert/advanced-git-search.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
     "natecraddock/workspaces.nvim",
@@ -15,33 +16,35 @@ local M = {
 }
 
 function M.config()
+  local trouble = require("trouble.providers.telescope")
   local telescope = require("telescope")
   local actions = require("telescope.actions")
   local themes = require("telescope.themes")
 
   telescope.setup({
     defaults = {
-      file_ignore_patterns = {
-        "node%_modules",
-        "target",
-        "classes",
-        ".class",
-        ".cpcache",
-        "cache",
-        ".git",
-        ".clj-kondo",
-        "tmp",
-        "shadow-cljs",
-        "-lock.*"
-      },
+      -- file_ignore_patterns = {
+      --   "node%_modules",
+      --   "target",
+      --   "classes",
+      --   ".class",
+      --   ".cpcache",
+      --   "cache",
+      --   ".git",
+      --   ".clj-kondo",
+      --   "tmp",
+      --   "shadow-cljs",
+      --   "-lock.*",
+      -- },
       mappings = {
         n = {
           ["q"] = actions.close,
+          ["<c-t>"] = trouble.open_with_trouble,
         },
         i = {
-          -- ["<esc><esc>"] = actions.close,
           ["<C-j>"] = actions.move_selection_next,
           ["<C-k>"] = actions.move_selection_previous,
+          ["<c-t>"] = trouble.open_with_trouble,
         },
       },
       vimgrep_arguments = {
@@ -49,17 +52,15 @@ function M.config()
         -- search all known types
         "--type=all",
         -- ignore this dirs
-        "--glob=!*/.git/*",
-        "--glob=!*/node_modules/*",
-        "--glob=!.cpcache/*",
-        "--glob=!*/.shadow-cljs/*",
-        "--glob=!*/.cache/*",
-        "--glob=!*/target/*",
-        "--glob=!*/tmp/*",
+        "--glob=!**/.git/*",
+        "--glob=!**/node_modules/*",
+        "--glob=!**/.next/*",
+        "--glob=!**/target/*",
+        "--glob=!**/.shadow-cljs/*",
+        "--glob=!**/.cpcache/*",
+        "--glob=!**/.cache/*",
+        "--glob=!*-lock.*",
         "--glob=!*.log",
-        "--glob=!*-lock.json",
-        "--glob=!*-lock.yml",
-        "--glob=!*-lock.yaml",
         -- limit of sin, lines longer than 150 is not relevant
         "--max-columns=150",
         "--hidden",
@@ -99,7 +100,31 @@ function M.config()
         -- Other telescope configuration options
       },
     },
+    pickers = {
+      find_files = {
+        find_command = {
+          "rg",
+          "--files",
+          "--hidden",
+          "--ignore",
+          "-u",
+          "--trim",
+          "--smart-case",
+          "--max-columns=150",
+          "--glob=!**/.git/*",
+          "--glob=!**/node_modules/*",
+          "--glob=!**/.next/*",
+          "--glob=!**/target/*",
+          "--glob=!**/.shadow-cljs/*",
+          "--glob=!**/.cpcache/*",
+          "--glob=!**/.cache/*",
+          "--glob=!*-lock.*",
+          "--glob=!*.log",
+        },
+      },
+    },
   })
+  telescope.load_extension("advanced_git_search")
   telescope.load_extension("ui-select")
   telescope.load_extension("fzf")
   telescope.load_extension("workspaces")
