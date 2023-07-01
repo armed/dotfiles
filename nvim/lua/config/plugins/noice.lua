540c7b820dfb70b6c06e977703636787b4aa9e38
Artem Medeu <artem.medeusheyev@gmail.com>
2023-07-01 21:09:34 +0600

    nvim: switch noice to repo

--- nvim/lua/config/plugins/noice.lua
+++ nvim/lua/config/plugins/noice.lua
@@ -1,6 +1,6 @@
 return {
-  dir = "~/Developer/neovim/noice.nvim",
-  -- "folke/noice.nvim",
+  -- dir = "~/Developer/neovim/noice.nvim",
+  "folke/noice.nvim",
   enabled = true,
   event = "VeryLazy",
   opts = {
