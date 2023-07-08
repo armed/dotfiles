378c9170bd5efa8d610535b13d42d5de61255402
Artem Medeu <artem.medeusheyev@gmail.com>
2023-07-08 16:02:55 +0400

    nvim: remove folds from statuscol

--- nvim/lua/config/plugins/statuscol.lua
+++ nvim/lua/config/plugins/statuscol.lua
@@ -6,13 +6,14 @@ return {
     local statuscol = require("statuscol")
     statuscol.setup({
       relculright = true,
+      ft_ignore = { "neo-tree", "help", "alpha" },
       segments = {
         {
-          sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+          sign = { name = { "Diagnostic" }, maxwidth = 1, auto = false },
           click = "v:lua.ScSa",
         },
-        { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-        { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+        { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+        -- { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
         {
           sign = {
             name = { ".*" },
