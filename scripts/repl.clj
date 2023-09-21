45f990381f724097d53be54ac3bcbeacc3226616
Artem Medeu <artem.medeusheyev@gmail.com>
2023-09-21 13:44:15 +0600

    scripts: repl now create pom.xml for jdtls

--- scripts/repl.clj
+++ scripts/repl.clj
@@ -1,12 +1,12 @@
 #!/usr/bin/env bb
-
 (ns repl
   (:require
    [babashka.fs :as fs]
    [babashka.process :as bp]
    [clojure.edn :as edn]
    [clojure.java.io :as io]
-   [clojure.string :as string]))
+   [clojure.string :as string]
+   [clojure.tools.build.api :as b]))
 
 (defn dirname []
   (let [current-path (fs/cwd)
@@ -16,7 +16,7 @@
 (def lsp-config-path ".lsp/config.edn")
 (def aliases-file ".repl_aliases")
 
-(def default-aliases #{"cider" "dev" "portal"})
+(def default-aliases #{"cider" "dev" "portal" "scope-capture"})
 
 (def project-aliases (when-let [deps (some-> "deps.edn"
                                              (slurp)
@@ -72,7 +72,47 @@
 
 (defn clean!
   []
+  (bp/shell "rm -f pom.xml")
   (bp/shell "rm -rf .cpcache"))
+
+(defn basis->mvn-deps
+  [basis]
+  (let [libs (filter (fn [[_ props]]
+                       (= :mvn (:deps/manifest props)))
+                     (-> basis :libs))]
+    (map (fn [[lib props]]
+           (let [group-id (or (namespace lib)
+                              (name lib))
+                 artifact-id (name lib)
+                 version (:mvn/version props)]
+             (str "    <dependency>\n"
+                  "      <groupId>" group-id "</groupId>\n"
+                  "      <artifactId>" artifact-id "</artifactId>\n"
+                  "      <version>" version "</version>\n"
+                  "    </dependency>")))
+         libs)))
+
+(defn write-pom!
+  [aliases]
+  (let [basis (b/create-basis {:project "deps.edn"
+                               :aliases aliases})]
+    (with-out-str (b/write-pom {:basis basis
+                                :lib 'armed/dev
+                                :target "."
+                                :version "0.0.0"}))
+    (let [pom-data (-> (slurp "pom.xml") (string/split-lines))
+          pre-deps (take-while (fn [s]
+                                 (-> s string/trim (not= "<dependencies>")))
+                               pom-data)
+          post-deps (drop 1 (drop-while (fn [s]
+                                          (-> s string/trim (not= "</dependencies>")))
+                                        pom-data))
+          deps (basis->mvn-deps basis)]
+      (spit "pom.xml" (string/join "\n" (concat pre-deps
+                                                ["  <dependencies>"]
+                                                deps
+                                                ["  </dependencies>"]
+                                                post-deps))))))
 
 (if (seq project-aliases)
   (try
@@ -96,12 +136,13 @@
           cmd (str "clojure -M" (string/join selected-aliases))]
       (save-aliases! selected-aliases)
       (clean!)
+      (write-pom! selected-aliases)
       (println cmd)
       (bp/shell (str "echo \"\033]0;" (dirname) "[repl]\007\""))
       (bp/shell cmd))
     (catch Exception e
       (let [data (ex-data e)]
         (when-not (= 130 (:exit data))
-          e))))
+          (print (ex-message e))))))
   (println "No local deps.edn found, bye"))
 
