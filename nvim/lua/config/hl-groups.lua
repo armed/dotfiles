daf9c58f6b678afd3170edefe5ef3a69593a87d0
Artem Medeu <artem.medeusheyev@gmail.com>
2023-09-21 13:47:13 +0600

    nvim: remove workaround

--- nvim/lua/config/hl-groups.lua
+++ nvim/lua/config/hl-groups.lua
@@ -2,9 +2,9 @@ local function init()
   vim.api.nvim_command("hi link IlluminatedWordRead LspReferenceRead")
   vim.api.nvim_command("hi link IlluminatedWordWrite LspReferenceWrite")
   vim.api.nvim_command("hi link IlluminatedWordText LspReferenceRead")
-  -- NOTE: workaround after treesitter update (wait for kanagawa update I guess)
-  vim.api.nvim_command("hi DoNothing guifg=NONE guibg=NONE gui=NONE cterm=NONE")
-  vim.api.nvim_command("hi link @field.clojure DoNothing")
+  -- -- NOTE: workaround after treesitter update (wait for kanagawa update I guess)
+  -- vim.api.nvim_command("hi DoNothing guifg=NONE guibg=NONE gui=NONE cterm=NONE")
+  -- vim.api.nvim_command("hi link @field.clojure DoNothing")
 
   vim.api.nvim_command(
     "hi user.repl.winbar guibg=#ffaa88 guifg=black cterm=bold"
