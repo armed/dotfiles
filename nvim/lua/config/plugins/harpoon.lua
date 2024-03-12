caaf50ad46b4abd7ae1a2f5647a1909308cd52c1
Artem Medeu <artem.medeusheyev@gmail.com>
2024-03-12 11:41:45 +0500

    nvim: update harpoon to v2

--- nvim/lua/config/plugins/harpoon.lua
+++ nvim/lua/config/plugins/harpoon.lua
@@ -1,19 +1,98 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   keys = {
-    { "<leader>b", desc = "Harpoon" }
+    { "<leader>b", desc = "Harpoon" },
+  },
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
   },
   config = function()
+    local h = require("harpoon")
     local wk = require("which-key")
-    local hm = require("harpoon.mark")
-    local hui = require("harpoon.ui")
+    local conf = require("telescope.config").values
+
+    h:setup({
+      settings = {
+        save_on_toggle = true,
+      },
+    })
+
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
 
     wk.register({
       b = {
-        a = { hm.add_file, "Mark File" },
-        b = { hui.toggle_quick_menu, "Menu" },
-        l = { hui.nav_next, "Next File" },
-        h = { hui.nav_prev, "Prev File" },
+        a = {
+          function()
+            h:list():append()
+          end,
+          "Mark File",
+        },
+        b = {
+          function()
+            h.ui:toggle_quick_menu(h:list())
+          end,
+          "Harpoon",
+        },
+        f = {
+          function()
+            toggle_telescope(h:list())
+          end,
+          "Menu",
+        },
+        ["1"] = {
+          function()
+            h:list():select(1)
+          end,
+          "Select 1",
+        },
+        ["2"] = {
+          function()
+            h:list():select(2)
+          end,
+          "Select 2",
+        },
+        ["3"] = {
+          function()
+            h:list():select(3)
+          end,
+          "Select 3",
+        },
+        ["4"] = {
+          function()
+            h:list():select(4)
+          end,
+          "Select 4",
+        },
+        l = {
+          function()
+            h:list():next()
+          end,
+          "Next File",
+        },
+        h = {
+          function()
+            h:list():prev()
+          end,
+          "Prev File",
+        },
       },
     }, { prefix = "<leader>" })
   end,
