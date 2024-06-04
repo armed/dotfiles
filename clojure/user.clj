b866e6feca2b079edd7fa59297ac54209b5d3765
Artem Medeu <artem.medeusheyev@gmail.com>
2024-06-04 08:54:20 +0500

    clojure: local dev user ns

---
+++ clojure/user.clj
@@ -0,0 +1,30 @@
+(ns user
+  (:require [clojure.edn :as edn]))
+
+(defonce local-dev* (atom nil))
+
+(defn load-alias []
+  (or @local-dev*
+      (some->> (slurp "deps.local.edn")
+               (edn/read-string)
+               :aliases
+               :local-dev
+               (reset! local-dev*))))
+
+(defn reset-app! []
+  (if-let [sym (some-> (load-alias) :reset-fn)]
+    (let [f (requiring-resolve sym)]
+      (println f)
+      (f))
+    (println "No reset-fn FQN is specified in local-dev alias")))
+
+(defn stop-app! []
+  (if-let [sym (some-> (load-alias) :stop-fn)]
+    (let [f (requiring-resolve sym)]
+      (println f)
+      (f))
+    (println "No stop-fn FQN is specified in local-dev alias")))
+
+(print (str "\033[38;2;255;165;0m"
+            "Local dev init complete"
+            "\033[m\n"))
