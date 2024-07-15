246a03ee98e7c5afeecda7baac7c5ecfa85d86f8
Artem Medeu <artem.medeusheyev@gmail.com>
2024-07-15 12:31:31 +0500

    nvim: text-case shortcuts

--- nvim/lua/config/plugins/text-case.lua
+++ nvim/lua/config/plugins/text-case.lua
@@ -1,6 +1,20 @@
 return {
   "johmsalas/text-case.nvim",
-  enabled = false,
   event = "VeryLazy",
-  config = true
+  keys = {
+    "ga",
+    {
+      "ga.",
+      function ()
+        vim.cmd("TextCaseOpenTelescopeLSPChange")
+        vim.cmd("wa")
+      end,
+      mode = { "n", "x" },
+      desc = "Telescope",
+    },
+  },
+  config = function()
+    require("textcase").setup({ default_keymappings_enabled = false })
+    require("telescope").load_extension("textcase")
+  end,
 }
