ee845c7224d604eeaebdc47786cc15e5e368e639
Artem Medeu <artem.medeusheyev@gmail.com>
2024-09-06 18:46:38 +0500

    nvim: new plugins

---
+++ nvim/lua/config/plugins/cmake-tools.lua
@@ -0,0 +1,9 @@
+return {
+  "Civitasv/cmake-tools.nvim",
+  event = "VeryLazy",
+  config = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "akinsho/toggleterm.nvim",
+  },
+}
---
+++ nvim/lua/config/plugins/nvim-dap-ui.lua
@@ -0,0 +1,84 @@
+return {
+  "rcarriga/nvim-dap-ui",
+  event = "VeryLazy",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio",
+    "williamboman/mason.nvim",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  config = function()
+    local mason_path = require("mason-core.path")
+    local dap = require("dap")
+    local dapui = require("dapui")
+    local wk = require("which-key")
+
+    -- dap.adapters.lldb = {
+    --   type = "executable",
+    --   command = mason_path.bin_prefix("codelldb"),
+    --   name = "lldb",
+    -- }
+
+    dap.adapters.codelldb = {
+      type = "server",
+      host = "127.0.0.1",
+      port = "13777",
+      executable = {
+        command = mason_path.bin_prefix("codelldb"),
+        args = {
+          "--port",
+          "13777",
+        },
+      },
+    }
+    dapui.setup()
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    dap.configurations.cpp = {
+      {
+        name = "Launch",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input(
+            "Path to executable: ",
+            vim.fn.getcwd() .. "/",
+            "file"
+          )
+        end,
+        --program = '${fileDirname}/${fileBasenameNoExtension}',
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+      },
+    }
+
+    wk.add({
+      {
+        "<leader>d",
+        group = "Nvim Dap",
+      },
+      {
+        "<leader>db",
+        "<cmd>DapToggleBreakpoint<cr>",
+        desc = "Toggle Breakpoint",
+      },
+      {
+        "<leader>dr",
+        "<cmd>DapContinue<cr>",
+        desc = "Dap Continue",
+      },
+    })
+  end,
+}
