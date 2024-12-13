9de380928caec5792cd9c4a4c7d52e21232e5f26
Artem Medeu <artem.medeusheyev@gmail.com>
2024-12-13 14:41:36 +0500

    nvim: spectre replace engine

--- nvim/lua/config/plugins/spectre.lua
+++ nvim/lua/config/plugins/spectre.lua
@@ -2,6 +2,18 @@ return {
   "nvim-pack/nvim-spectre",
   cmd = "Spectre",
   dependencies = {
-    "nvim-lua/plenary.nvim"
-  }
+    "nvim-lua/plenary.nvim",
+  },
+  config = {
+    replace_engine = {
+      ["sed"] = {
+        cmd = "sed",
+        args = {
+          "-i",
+          "",
+          "-E",
+        },
+      },
+    },
+  },
 }
