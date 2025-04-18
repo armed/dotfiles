2f07662cf4fdd7f75a9604f9d037c23b70f5dc66
Artem Medeu <artem.medeusheyev@gmail.com>
2025-04-18 13:02:10 +0500

    nvim: blink snippets config

--- nvim/lua/config/plugins/blink.lua
+++ nvim/lua/config/plugins/blink.lua
@@ -1,35 +1,59 @@
 return {
   "saghen/blink.cmp",
   event = "InsertEnter",
-  -- optional: provides snippets for the snippet source
   dependencies = {
-    "rafamadriz/friendly-snippets",
-    "L3MON4D3/LuaSnip",
-    "onsails/lspkind.nvim",
+    "mikavilpas/blink-ripgrep.nvim",
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp",
+      dependencies = {
+        "rafamadriz/friendly-snippets",
+      },
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_snipmate").lazy_load({
+          paths = { vim.fn.stdpath("config") .. "/snippets" },
+        })
+        local extends = {
+          javascript = { "jsdoc" },
+          lua = { "luadoc" },
+          rust = { "rustdoc" },
+          java = { "javadoc" },
+          sh = { "shelldoc" },
+        }
+        -- friendly-snippets - enable standardized comments snippets
+        for ft, snips in pairs(extends) do
+          require("luasnip").filetype_extend(ft, snips)
+        end
+      end,
+      opts = { history = true, delete_check_events = "TextChanged" },
+    },
   },
 
-  -- use a release tag to download pre-built binaries
   version = "1.*",
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
+    enabled = function()
+      local disabled = false
+      local success, node = pcall(vim.treesitter.get_node)
+      disabled = disabled or (vim.bo.buftype == "prompt")
+      disabled = disabled or (vim.fn.reg_recording() ~= "")
+      disabled = disabled or (vim.fn.reg_executing() ~= "")
+      disabled = disabled
+        or (
+          success
+          and node ~= nil
+          and vim.tbl_contains(
+            { "comment", "line_comment", "block_comment" },
+            node:type()
+          )
+        )
+
+      return not disabled
+    end,
     keymap = {
       preset = "default",
       ["<S-Tab>"] = { "select_prev", "fallback" },
@@ -38,12 +62,9 @@ return {
     },
 
     appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = "mono",
     },
 
-    -- (Default) Only show the documentation popup when manually triggered
     completion = {
       documentation = {
         auto_show = true,
@@ -59,19 +80,31 @@ return {
         winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
       },
     },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    snippets = { preset = "luasnip" },
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "ripgrep" },
+      providers = {
+        ripgrep = {
+          module = "blink-ripgrep",
+          name = "Ripgrep",
+          ---@module "blink-ripgrep"
+          ---@type blink-ripgrep.Options
+          opts = {
+            prefix_min_len = 4,
+            score_offset = 10, -- should be lower priority
+            max_filesize = "300K",
+            search_casing = "--smart-case",
+          },
+        },
+      },
     },
 
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" },
+    fuzzy = {
+      implementation = "prefer_rust_with_warning",
+      use_frecency = false,
+      use_proximity = false,
+      sorts = { "exact", "score", "sort_text" },
+    },
     signature = { enabled = true },
   },
   opts_extend = { "sources.default" },
