df9fe031186387515afac4da606fa5a50427448e
Artem Medeu <artem.medeusheyev@gmail.com>
2026-01-12 11:47:48 +0500

    nvim: save all files on lsp rename

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -1,2 +1,13 @@
 require("config.lsp.diagnostics").setup()
 require("config.lsp.autocmds").setup()
+
+-- Custom rename handler that saves all files and refreshes current buffer
+local original_rename_handler = vim.lsp.handlers["textDocument/rename"]
+vim.lsp.handlers["textDocument/rename"] = function(...)
+  local result = original_rename_handler(...)
+  vim.cmd("wa")
+  vim.defer_fn(function()
+    vim.lsp.codelens.refresh()
+  end, 300)
+  return result
+end
